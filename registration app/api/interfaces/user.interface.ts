76c8a0cc9567be2f521e744652137180c57fbb14
Sithija Shehara <nsithijashehara@gmail.com>
2026-03-08 07:01:28 +0530

    removed registration app

--- registration app/api/interfaces/user.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Document, Types } from "mongoose";
-
-export interface IUser extends Document {
-  userNumber: number;
-  departmentNumber: number;
-  departmentName: string;
-  attendance: boolean;
-}
--- registration app/client/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export type registerInputs = {
-  userNumber: number;
-  departmentNumber: number;
-  departmentName: string;
-  attendance?: boolean;
-};
